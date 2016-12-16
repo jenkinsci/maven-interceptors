998732c323ad46390ada3309a61c27654d84a7cd
Jonathan Hyry <jon@ttcwenatchee.com>
2016-12-16 13:33:59 -0800

    Fix copy paste error
    Should use Maven33Main class.

--- maven33-agent/src/main/java/jenkins/maven3/agent/Maven33Main.java
+++ maven33-agent/src/main/java/jenkins/maven3/agent/Maven33Main.java
@@ -79,7 +79,7 @@ public class Maven33Main
 	    throws IOException, ConfigurationException, DuplicateRealmException, NoSuchRealmException {
         // load the default realms
         launcher = new Launcher();
-        launcher.setSystemClassLoader(Maven32Main.class.getClassLoader());
+        launcher.setSystemClassLoader(Maven33Main.class.getClassLoader());
         launcher.configure(getClassWorldsConfStream());
     }
 
