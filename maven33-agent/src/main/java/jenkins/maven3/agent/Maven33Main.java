d392849459933bd5fdd28a9e420985cb9eb9c651
Jonathan Hyry <jon@ttcwenatchee.com>
2016-12-16 13:21:46 -0800

    Apply Maven32Main fixes to Maven33Main
    The same issue appears to apply to jenkins.maven3.agent.Maven33Main

--- maven33-agent/src/main/java/jenkins/maven3/agent/Maven33Main.java
+++ maven33-agent/src/main/java/jenkins/maven3/agent/Maven33Main.java
@@ -26,6 +26,8 @@ package jenkins.maven3.agent;
 import org.codehaus.plexus.classworlds.launcher.Launcher;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.codehaus.plexus.classworlds.realm.NoSuchRealmException;
+import org.codehaus.plexus.classworlds.realm.DuplicateRealmException;
+import org.codehaus.plexus.classworlds.launcher.ConfigurationException;
 
 import java.io.*;
 import java.net.Socket;
@@ -72,7 +74,14 @@ public class Maven33Main
 
         main(m2Home, remotingJar, interceptorJar, interceptorCommonJar, null, tcpPort);
     }
-
+    
+    private static void initializeLauncher()
+	    throws IOException, ConfigurationException, DuplicateRealmException, NoSuchRealmException {
+        // load the default realms
+        launcher = new Launcher();
+        launcher.setSystemClassLoader(Maven32Main.class.getClassLoader());
+        launcher.configure(getClassWorldsConfStream());
+    }
 
     /**
      *
@@ -116,11 +125,7 @@ public class Maven33Main
         System.setProperty("maven3.interceptor", (interceptorJar != null ? interceptorJar
                 : interceptorJar).getPath());
 
-        // load the default realms
-        launcher = new Launcher();
-        launcher.setSystemClassLoader(Maven33Main.class.getClassLoader());
-        launcher.configure(getClassWorldsConfStream());
-
+        initializeLauncher();
 
         // create a realm for loading remoting subsystem.
         // this needs to be able to see maven.
@@ -173,6 +178,10 @@ public class Maven33Main
     public static int launch( String[] args ) throws Exception {
 
         try {
+            if (launcher == null) {
+                initializeLauncher();
+            }
+
             launcher.launch( args );
         } catch ( Throwable e ) {
             e.printStackTrace();
